040b753deb9141d8c3d0dcd1ebbe3c63874063dd
Airof <fmhmdairof@gmail.com>
2025-02-06 18:53:46 +0330

    register
    creating the core registers that will be used to create the mano machine registers in BUS

--- Common-Bus-System/REGISTER/core/JK_FlipFlop.vhd
+++ Common-Bus-System/REGISTER/core/JK_FlipFlop.vhd
@@ -11,13 +11,15 @@ entity JKff is
     );
 end entity;
 
-architecture behavioral of JKff is
+architecture Behave of JKff is
     signal q, q_next : std_logic := '0';
 begin
+
     q_next <= (not q) when (J = '1' and K = '1') else
               '1' when (J = '1' and K = '0') else
               '0' when (J = '0' and K = '1') else
               q;
+              
               
     q <= '0' when Clear = '1' else q_next when rising_edge(CLK);
     y <= q;
