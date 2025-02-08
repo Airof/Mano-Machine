c05730cddc7acad19a30a1824ff92c72c32e311a
Airof <fmhmdairof@gmail.com>
2025-02-09 01:02:02 +0330

    Main
    MANO machine main file

--- Common-Bus-System/BUS/Basic.vhd
+++ Common-Bus-System/BUS/Basic.vhd
@@ -20,9 +20,12 @@ architecture structural of Mano_machine is
     signal load: Register_Name;
     signal CLR: Register_Name;
     signal INC: Register_Name;
+    signal Wr: std_logic;
+
 
     -- BUS selectors
-    signal sel: std_logic_vector(2 downto 0);
+    signal X: std_logic_vector(7 downto 0); --encoded type for select
+    signal sel: std_logic_vector(2 downto 0); --decoded
     --Bus outputs
     signal on_BUS: std_logic_vector(15 downto 0);
     -- stores the ALU outpus
@@ -38,12 +41,21 @@ architecture structural of Mano_machine is
     signal I: std_logic;                     --regisrter and I/O
     signal R: std_logic;                     -- intrupt
 
+    signal IEN: std_logic;                   -- intrupt enable
+    signal FGO, FGI: std_logic;              -- I/O flags
 
+    signal TC: time_control;
+
+
+    -- 
+    signal memory_enable, R_flag: std_logic; --for read and write flag
 
 begin
-    
+-- ###############################################################    
+-- $$$$$$$$$the basic computer timing for sequential process$$$$$$$
+
     time_and_control: entity work.Timing port map(
-        IR => val.IR,
+        IR => value.IR,
         increment => '1',
         clear => clr.SC,
         clk => clk,
@@ -52,125 +64,245 @@ begin
         B => B,
         I => I
     );
--- load of the registers
-    load.PC <= 0;
-
-    load.AR <= 0;
-
-    load.IR <= 0;
-
-    load.TR <= 0;
-
-    load.DR <= 0;
-
-    load.AC <= 0;
 
 
 
--- load of the registers
-    CLR.AR <= 0;
-    
-    CLR.PC <= 0;
+-- #########################################################
+-- $$$$$$$$$computing the time and control signals for registers$$$$$$$
 
-    CLR.DR <= 0;
+    -- Assign all fields at once using an aggregate
+    TC <= (
+        -- Fetch and Decode
+        nRT0   => not R and T(0),
+        nRT1   => not R and T(1),
+        nRT2   => not R and T(2),
+        nD7IT3  => not D(7) and I and T(3),
+        RT0    => R and T(0),
+        RT1    => R and T(1),
+        RT2    => R and T(2),
 
-    CLR.AC <= 0;
-    
-    CLR.IR <= 0;
-    
-    CLR.TR <= 0;
- 
-     
+        -- Memory-reference
+        D0T4   => D(0) and T(4),
+        D0T5   => D(0) and T(5),
+        D1T4   => D(1) and T(4),
+        D1T5   => D(1) and T(5),
+        D2T4   => D(2) and T(4),
+        D2T5   => D(2) and T(5),
+        D3T4   => D(3) and T(4),
+        D4T4   => D(4) and T(4),
+        D5T4   => D(5) and T(4),
+        D5T5   => D(5) and T(5),
+        D6T4   => D(6) and T(4),
+        D6T5   => D(6) and T(5),
+        D6T6   => D(6) and T(6),
 
--- load of the registers
-    INC.AR <= 0;
+        -- Register-reference
+        r      => D(7) and I and T(3),
+        rB11   => TC.r and B(11),
+        rB10   => TC.r and B(10),
+        rB9    => TC.r and B(9),
+        rB8    => TC.r and B(8),
+        rB7    => TC.r and B(7),
+        rB6    => TC.r and B(6),
+        rB5    => TC.r and B(5),
+        rB4    => TC.r and B(4),
+        rB3    => TC.r and B(3),
+        rB2    => TC.r and B(2),
+        rB1    => TC.r and B(1),
+        rB0    => TC.r and B(0),
 
-    INC.PC <= 0;
-
-    INC.DR <= 0;
-    
-    INC.AC <= 0;
-    
-    INC.IR <= 0;
-    
-    INC.TR <= 0;
-
-
-
-    ALU_unit: entity work.ALU port map(
-        
-        
+        -- Input-Output
+        p    => D(7) and (not I) and T(3),
+        pB11 => TC.p and B(11),
+        pB10 => TC.p and B(10),
+        pB9  => TC.p and B(9),
+        pB8  => TC.p and B(8),
+        pB7  => TC.p and B(7),
+        pB6  => TC.p and B(6)
     );
+
+-- #########################################################
+-- $$$$$$ choose and decode the selectors for Bus $$$$$$$$$$
+
+    -- X(0) <= '0'; -- not selected 
+
+    X(1) <= '0'; -- AR
+
+    X(2) <= TC.nRT0 OR TC.RT0; -- PC
+
+    X(3) <= '0'; -- DR
+
+    X(4) <= '0'; -- AC
+
+    X(5) <= TC.nRT2; -- IR
+
+    X(6) <= TC.RT1; -- TR
+
+    X(7) <= TC.nRT1 OR TC.ND7IT3; -- MEMORY//also use as read
+
+
+
+    select_selector: entity work.enc8to3 
+        port map (
+            input => X,
+            output => sel
+        );
+-- #########################################################
+-- $$$$$$ finding Effective address $$$$$$$$$
+
+
+
+
+
+-- load of the registers
+    load.AR <= TC.nRT0 OR TC.nRT2 OR TC.ND7IT3;
+
+    load.PC <= '0';
+
+    load.DR <= '0';
+    
+    load.AC <= '0';
+    
+    load.IR <= TC.nRT1;
+    
+    load.TR <= TC.RT0;
+
+    wr <= TC.RT1;
+
+
+
+-- CLEAR of the registers
+    CLR.AR <= TC.RT0;
+    
+    CLR.PC <= TC.RT1;
+
+    CLR.DR <= '0';
+
+    CLR.AC <= '0';
+    
+    CLR.IR <= '0';
+    
+    CLR.TR <= '0';
+ 
+    -- clear of the sequence counter
+    clr.SC <= clr.SC and not TC.RT2;
+
+-- INCREMENT of the registers
+    INC.AR <= '0';
+
+    INC.PC <= TC.nRT1 OR TC.RT2;
+
+    INC.DR <= '0';
+    
+    INC.AC <= '0';
+    
+    INC.IR <= '0';
+    
+    INC.TR <= '0';
+
+
+
+-- Handling the intrupt flags
+
+    -- intrupt check
+    R <= ( ((not (T(0) OR T(1))) OR T(2)) and IEN and (FGI OR FGO) ) OR ( R and TC.RT2 );
+
+
+    -- inrupt enable 
+    IEN <= (IEN and not TC.RT2);
+
+
+
+
+    -- ALU_unit: entity work.ALU port map(
+        
+        
+    -- );
 
 -- $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
 -- ########### REGISTERS #######################
-    AR_reg: entity work.reg16 
+    AR_reg: entity work.reg12 
         port map(
             count => INC.AR,
             load => load.AR,
-            I => on_BUS,
+            clear => clr.AR,
+            I => on_BUS(11 downto 0),
             clk => clk,
             A_out => value.AR,
-            cout => open
+            C_out => open
         );
 
-    PC_reg: entity work.reg16 
+    PC_reg: entity work.reg12 
         port map(
             count => INC.PC,
             load => load.PC,
-            I => on_BUS,
+            clear => clr.PC,
+            I => on_BUS(11 downto 0),
             clk => clk,
             A_out => value.PC,
-            cout => open
+            C_out => open
         );
 
     DR_reg: entity work.reg16 
         port map(
             count => INC.DR,
             load => load.DR,
+            clear => clr.DR,
             I => on_BUS,
             clk => clk,
             A_out => value.DR,
-            cout => open
+            C_out => open
         );
 
     AC_reg: entity work.reg16 
         port map(
             count => INC.AC,
             load => load.AC,
-            I => ALU_output,
+            clear => clr.AC,
+            I => ALU_output(15 downto 0),
             clk => clk,
             A_out => value.AC,
-            cout => open
+            C_out => open
         );
+        E <= ALU_output(16);
+
 
     IR_reg: entity work.reg16 
         port map(
             count => INC.IR,
             load => load.IR,
+            clear => clr.IR,
             I => on_BUS,
             clk => clk,
             A_out => value.IR,
-            cout => open
+            C_out => open
         );
 
     TR_reg: entity work.reg16 
         port map(
             count => INC.TR,
             load => load.TR,
+            clear => clr.TR,
             I => on_BUS,
             clk => clk,
             A_out => value.TR,
-            cout => open
+            C_out => open
         );
 
 
 -- $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
 -- ############## RAM ##########################
 
-    -- Memory: entity work.RAM
-    --     port map(
-    --         addr: E_addr
-    --     );
+    memory_enable <=  X(7) XOR WR;
+    R_flag  <=  (not X(7)) and WR;
+
+    Memory: entity work.RAM
+        port map(
+            addr =>  value.AR,
+            input_data => on_BUS,
+            output_data => on_BUS,
+            memory_enable => memory_enable, --to check RD and WR won't be high at the same time
+            R_flag => R_flag
+        );
 
 end architecture structural;
--- Common-Bus-System/packages/Basic_pkg.vhd
+++ Common-Bus-System/packages/Basic_pkg.vhd
@@ -1,7 +1,5 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.STD_LOGIC_ARITH.ALL;
-use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 package Bus_Pkg is
 
@@ -30,6 +28,20 @@ package Bus_Pkg is
     component reg16 is
         generic(
             size: integer := 16
+        );
+        port(
+            Count, load : in std_logic;
+            I : in std_logic_vector(size-1 downto 0);  -- Changed to 7 bits
+            Clk, Clear : in std_logic;
+            A_out : out std_logic_vector(size-1 downto 0);  -- Changed to 7 bits
+            C_out : out std_logic
+        );
+    end component;
+
+
+    component reg12 is
+        generic(
+            size: integer := 12
         );
         port(
             Count, load : in std_logic;
@@ -75,14 +87,34 @@ package Bus_Pkg is
     end component Bus7;
 
 
+    component enc8to3 is
+        port (
+            input: in std_logic_vector(7 downto 0);
+            output: out std_logic_vector(2 downto 0)
+        );
+    end component enc8to3;
+
+    component MUX2to1 is
+        port (
+            input : in std_logic_vector(1 downto 0);
+            sel : in std_logic;
+            q : out std_logic
+        );
+    end component MUX2to1;
+    
+    
+
+
         -- Define the record type inside the architecture
     type Register_Values is record    
         AC  : std_logic_vector(15 downto 0);
-        AR  : std_logic_vector(15 downto 0);
         DR  : std_logic_vector(15 downto 0);
         IR  : std_logic_vector(15 downto 0);
-        PC  : std_logic_vector(15 downto 0);
+        AR  : std_logic_vector(11 downto 0);
+        PC  : std_logic_vector(11 downto 0);
         TR  : std_logic_vector(15 downto 0);
+        OUTR: std_logic_vector(7 downto 0);
+        INPR: std_logic_vector(7 downto 0);
     end record;    
 
     type Register_Name is record
@@ -95,4 +127,50 @@ package Bus_Pkg is
         SC  : std_logic;
     end record;        
 
+    type Time_Control is record
+        --fetch and decode
+        nRT0   : std_logic;
+        nRT1   : std_logic;
+        nRT2   : std_logic;
+        nD7IT3  : std_logic;
+        RT0    : std_logic;
+        RT1    : std_logic;
+        RT2    : std_logic; -- seems that RT2 is reseaved ???
+        -- Memory-reference
+        D0T4   : std_logic;
+        D0T5   : std_logic;
+        D1T4   : std_logic;
+        D1T5   : std_logic;
+        D2T4   : std_logic;
+        D2T5   : std_logic;
+        D3T4   : std_logic;
+        D4T4   : std_logic;
+        D5T4   : std_logic;
+        D5T5   : std_logic;
+        D6T4   : std_logic;
+        D6T5   : std_logic;
+        D6T6   : std_logic;
+        -- Register-reference
+        r      : std_logic;
+        rB11   : std_logic;
+        rB10   : std_logic;
+        rB9    : std_logic;
+        rB8    : std_logic;
+        rB7    : std_logic;
+        rB6    : std_logic;
+        rB5    : std_logic;
+        rB4    : std_logic;
+        rB3    : std_logic;
+        rB2    : std_logic;
+        rB1    : std_logic;
+        rB0    : std_logic;
+        -- Input-Output
+        p      : std_logic;
+        pB11   : std_logic;
+        pB10   : std_logic; 
+        pB9    : std_logic;
+        pB8    : std_logic;
+        pB7    : std_logic;
+        pB6    : std_logic;
+    end record;
 end package Bus_Pkg;
